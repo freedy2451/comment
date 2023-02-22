e4be714547717a15d0eff816ad878085b2aead72
Lucy <b237237@gmail.com>
2023-02-22 23:13:53 +0800

    v0.03

--- comment.py
+++ comment.py
@@ -13,4 +13,13 @@ print("檔案讀取完了，總共有{}筆資料".format(len(data)))
 sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
-print("留言平均長度是", sum_len/len(data))    
+print("留言平均長度是", sum_len/len(data))   
+
+#留言長度篩選
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print("一共有", len(new), "筆留言小於100")        
+print(new[0])
+print(new[1])
