a66f1ce9bf6698c971afb8b018b0690bf51f6eb0
Lucy <b237237@gmail.com>
2023-03-12 21:46:24 +0800

    'function'

--- comment.py
+++ comment.py
@@ -10,6 +10,7 @@ with open('reviews.txt', 'r') as f:
 #print(data[0])        
 print("檔案讀取完了，總共有{}筆資料".format(len(data)))
 
+
 sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
@@ -36,4 +37,29 @@ print(good[0])
 
 
 print("一共有{}筆留言提到good".format(len(good)))     
-print(new[0])   
+print(new[0])
+
+#文字記數
+wc = {} #word count
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1 #新增KEY進字典   
+for word in wc:
+    if wc[word] > 100:
+        print(wc[word])
+
+while True:
+    a = input('請問你想查什麼字: ')        
+    if a == 'q':
+        print('感謝使用')
+        break 
+    if a in wc:    
+        print(a, '出現次數為: ', wc[a])
+    else:
+        print('這個字沒出現過')    
+        
+    
